aea011bd3dd75841053e2624f57e13d7e14c6948
petercla0119 <petersonc0119@gmail.com>
2024-08-14 23:44:54 -0400

    confirmed updated v10 parser works in IDE

--- parser_files/dual_guide_parser_v10-2.py
+++ parser_files/dual_guide_parser_v10-2.py
@@ -156,21 +156,25 @@ check_reverse = True
 """# 1. Import data, this includes concensus guides and R1 + R2 fastqs."""
 
 # Function to read the guide library file in .csv and .txt format
-def read_guides_file(guides_file):
-    file_extension = os.path.splitext(guides_file)[1].lower()
+# TODO: Ensure function runs
+# def read_guides_file(guides_file):
+#     file_extension = os.path.splitext(guides_file)[1].lower()
+#
+#     if file_extension == '.txt':
+#         # Read as a tab-delimited file
+#         guides_df = pd.read_csv(guides_file, sep = '\t', engine = 'c')
+#     elif file_extension == '.csv':
+#         # Read as a comma-separated file
+#         guides_df = pd.read_csv(guides_file, engine = 'c')
+#     else:
+#         raise ValueError("Unsupported file type: {}".format(file_extension))
+#
+#     return guides_df
+#
+# guides_df = read_guides_file(guides_file)
 
-    if file_extension == '.txt':
-        # Read as a tab-delimited file
-        guides_df = pd.read_csv(guides_file, sep = '\t', engine = 'c')
-    elif file_extension == '.csv':
-        # Read as a comma-separated file
-        guides_df = pd.read_csv(guides_file, engine = 'c')
-    else:
-        raise ValueError("Unsupported file type: {}".format(file_extension))
-
-    return guides_df
-
-guides_df = read_guides_file(guides_file)
+# Modify based on guide file format - Guide file received was in txt format
+guides_df = pd.read_csv(guides_file, sep='\t')
 
 """# # Import R1s and R2s.
 # ## pysam way Pysam introduces many other file format compatibilities such as
